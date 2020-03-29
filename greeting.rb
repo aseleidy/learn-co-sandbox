95232886ef2a35ed947b5af29be53d6d9d31bdae
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-29 15:22:06 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,5 @@
+def greeting_a_person(name, language)
+  puts "Hello #{name}. I know you code in #{language}"
+end
+
+greeting_a_person("Maria", "ruby")
---
+++ learning-if-end.rb
@@ -0,0 +1,20 @@
+#run_code_inside = false
+#puts "Code before if...end"
+#if run_code_inside
+#  puts "code inside"
+#end
+#puts "Code after if...end"
+
+#count = 0
+#while count <= 2 do
+#  puts "say this forever..."
+#  count += 1
+#end
+
+def say_hello_five_times
+  5.times do 
+    puts "Hello World!"
+  end
+end
+
+say_hello_five_times
